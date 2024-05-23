a47bee9dbe7b9bf59fc39e4e06793629d9700b28
Aaron <119818687+mustangwaves@users.noreply.github.com>
2024-05-22 22:57:47 -0400

    Update rockPaperScissors.py
    Added a play again function.

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -16,25 +16,33 @@ def write_balance(balance):
 
 balance = int(balance)
 
+play_again = True
+#Loop to allow player to play multiple times
+while play_again == True:
+    
 # gets computers choice and converts it to rock, paper, or scissors
 
-computerChoice = random.choice(['Rock','Paper','Scissors'])
+    computerChoice = random.choice(['Rock','Paper','Scissors'])
 
-bet = int(input('How much would you like to bet?'))
-playerChoice = input('Rock, Paper, or Scissors?')
+    bet = int(input('How much would you like to bet?'))
+    playerChoice = input('Rock, Paper, or Scissors?')
 
 # prints out results of the game
 
-if playerChoice == computerChoice:
-    print(f"the computer chose {computerChoice}. It's a draw!")
-elif (playerChoice == 'Rock' and computerChoice == 'Scissors') or \
-    (playerChoice == 'Paper' and computerChoice == 'Rock') or \
-    (playerChoice == 'Scissors' and computerChoice == 'Paper'):
-        print(f"The computer chose {computerChoice}. You win!")
-        balance += bet
-else:
-    print(f"The computer chose {computerChoice}. You lose!")
-    balance -= bet
+    if playerChoice == computerChoice:
+        print(f"the computer chose {computerChoice}. It's a draw!")
+    elif (playerChoice == 'Rock' and computerChoice == 'Scissors') or \
+        (playerChoice == 'Paper' and computerChoice == 'Rock') or \
+        (playerChoice == 'Scissors' and computerChoice == 'Paper'):
+            print(f"The computer chose {computerChoice}. You win!")
+            balance += bet
+    else:
+        print(f"The computer chose {computerChoice}. You lose!")
+        balance -= bet
 
-print(f'Your new balance is: {balance}')
-write_balance(balance)
+    print(f'Your new balance is: {balance}')
+    write_balance(balance)
+
+    playAgain = input('Play again? (y/n)')
+    if playAgain == 'n':
+        play_again = False
